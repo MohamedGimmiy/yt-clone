b6c34527a1e5066dc1293fd83a730c30635750ab
Mohamed Hussin <mohamedhussin07@gmail.com>
2023-01-03 08:58:05 +0200

    append video grid to channel index

--- resources/views/channel/index.blade.php
+++ resources/views/channel/index.blade.php
@@ -8,12 +8,46 @@
     </div>
 </div>
 <div class="container">
-    <div class="d-flex align-items-center">
-        <img src="{{asset('/images/' . $channel->image)}}" class="rounded-circle mr-3" height="130px">
-        <div>
-            <h3>{{$channel->name}}</h3>
-            <p>{{$channel->subscribers()}} Subscribers</p>
+    <div class="d-flex justify-content-between align-items-center">
+        <div class="d-flex align-items-center">
+            <img src="{{asset('/images/' . $channel->image)}}" class="rounded-circle mr-3" height="130px">
+            <div>
+                <h3>{{$channel->name}}</h3>
+                <p>{{$channel->subscribers()}} Subscribers</p>
 
+            </div>
+        </div>
+        <div>
+            @can('update',$channel)
+            <a href="{{route('channel.edit', $channel)}}" class="btn btn-primary">Edit Channel</a>
+            @endcan
+        </div>
+    </div>
+    <div>
+        <div class="row my-4">
+
+            @foreach ($channel->videos as $video)
+            <div class="col-12 col-md-6 col-lg-4">
+                <a style="text-decoration: none;" href="{{route('video.watch' , $video)}}" class="card-link">
+
+                    <div class="carb mb-4" style="width:333px; border:none;">
+                        <img class="card-img-top" src="{{asset($video->thumbnail)}}" alt="Card image cap"
+                        style="height: 174px; width:333px">
+
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <img src="{{asset('/images/' . $video->channel->image)}}" height="40px"
+                                class="rounded-circle" alt="">
+                                <h4 class="ml-3">{{$video->title}}</h4>
+                            </div>
+                        </div>
+                        <p class="text-gray mt-4 font-weight-bold" style="line-height: 0.2px">{{$video->channel->name}}</p>
+                        <p class="text-gray font-weight-bold"  style="line-height: 0.2px">{{$video->views}} view .
+                        {{$video->created_at->diffForHumans()}}</p>
+                    </div>
+                </a>
+            </div>
+            @endforeach
         </div>
     </div>
 </div>
